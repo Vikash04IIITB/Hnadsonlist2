ff89cc8154c1de9b5f5374ab3be2941b7addaef3
Vikash04IIITB <vikjaisha2@gmail.com>
2024-09-23 20:56:45 +0530

    hol2

---
+++ HANDSONLIST2/22-global.h
@@ -0,0 +1,13 @@
+//Vikash Kumar Jaiswal
+//MT2024168
+#include <errno.h>
+char *fifoName = "./22-fifo";
+
+void init()
+{
+    int fifoStatus;
+
+    fifoStatus = mkfifo(fifoName, S_IRWXU);
+    if (!(errno == EEXIST || fifoStatus != -1))
+        perror("Error while creating FIFO file fifoOne");
+}
